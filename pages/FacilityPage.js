b4e967ccd0046866c949a2ebbeb2b340a92ab1f4
Michael Johnson <mrj0730@icloud.com>
2020-05-02 12:36:28 -0500

    begin skeleton layout, pay table logic complete

---
+++ pages/FacilityPage.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const FacilityPage = () => 
+  (
+  <div id="">
+    <header>
+      <h1>FacID</h1>
+      <p>FacName</p>
+    </header>
+
+    <main>
+      <section id="fac-info">
+
+
+      </section>
+
+      <section id="fac-pay">
+
+
+      </section>
+
+    </main>
+
+  </div>
+  )
+
+export default FacilityPage
