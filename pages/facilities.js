a7aba0539ebd5dda2f76990aa8fe0c0e75e44caf
Michael Johnson <mrj0730@icloud.com>
2020-05-09 00:54:59 -0500

    adding pages skeletons; added some css

---
+++ pages/facilities.js
@@ -0,0 +1,20 @@
+import Layout from '../components/layout';
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import * as facs from '../scripts/facility_info';
+
+const Facilites = () => {
+
+  return (
+    <Layout>
+      <Head>
+        <title>pointSixtyFive - Facility Information</title>
+      </Head>
+
+    <p>Placeholder</p>
+
+
+    </Layout>
+)}
+
+export default Facilites;
