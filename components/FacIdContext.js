e592f3b852bcad47a04ae869e6e211fdf6b16ebb
Michael Johnson <mrj0730@icloud.com>
2020-05-10 15:59:27 -0500

    context for facID propagating correctly

---
+++ components/FacIdContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+export const FacIdContext = createContext();
+
+const FacIdContextProvider = (props) => {
+  const [ FacId, setFacId ] = useState('');
+  const storeFacId = FacId => {
+    setFacId(FacId)
+  }
+ 
+
+  return (
+    <FacIdContext.Provider value={[FacId, storeFacId]}>
+      {props.children}
+    </FacIdContext.Provider>
+  )
+}
+export default FacIdContextProvider;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles.css'
+import '../styles.css';
+import Layout from '../components/layout';
+import FacIdContextProvider from '../components/FacIdContext';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <FacIdContextProvider>
+    <Layout>
+        <Component {...pageProps} />
+    </Layout>
+      </FacIdContextProvider>
+  )
 }
