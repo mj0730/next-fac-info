f17074e489e24a35991de6ae9845c0c486263b71
Michael Johnson <mrj0730@icloud.com>
2020-05-11 20:15:20 -0500

    setting front page display logic, need to include new state to display front page or not

---
+++ components/FrontPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const FrontPage = () => {
+
+  return (
+    <main>
+      <Head>
+        <title>pointSixtyFive - Facility Information</title>
+      </Head>
+
+      <p>front page text</p>
+    </main>
+  )
+}
+
+export default FrontPage;
