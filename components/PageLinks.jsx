c9ce9079419377d899f224cefc026a102a1cbc46
Michael Johnson <mrj0730@icloud.com>
2020-05-11 23:30:06 -0500

    front page logic and links complete

--- components/PageLinks.jsx
+++ components/PageLinks.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Link from 'next/link';
+import {FacIdContext} from './FacIdContext';
 
 import HomeIcon from '@material-ui/icons/Home';
 import TrackChangesIcon from '@material-ui/icons/TrackChanges';
 import MapIcon from '@material-ui/icons/Map';
 
 const PageLinks = (props) => {
+  const [FacId, storeFacId, displayFrontPage, setDisplayFrontPage] = useContext(FacIdContext);
+
+  const handleClick = () => {
+    setDisplayFrontPage(true);
+    storeFacId('')
+  }
 
   return (
     <nav id="page-links">
       <ul>
-        <li><Link href="/"><a><HomeIcon/> Home</a></Link></li>
+        <li><Link href="/"><a onClick={handleClick}><HomeIcon/> Home</a></Link></li>
         <li> <Link href="/facilities"><a><TrackChangesIcon/> Facilities</a></Link></li>
         <li><Link href="/map"><a><MapIcon/> Map</a></Link></li>
       </ul>
