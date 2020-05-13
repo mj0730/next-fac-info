0d2b8917705c6b09aa84ff401319c011bb7dbd7c
Michael Johnson <mrj0730@icloud.com>
2020-05-12 22:03:59 -0500

    table layout complete; need styling and finish configuring options

--- pages/facilities.js
+++ pages/facilities.js
@@ -1,23 +1,65 @@
 import Head from 'next/head';
-import { useState, useEffect, useContext } from 'react';
-import * as facs from '../scripts/facility_info';  //refactor to only import needed object
+import { useState, useEffect, useContext, forwardRef} from 'react';
+import {FACILITIES} from '../scripts/facility_info';
 import FacilityListItem from '../components/FacilityListItem';
+import MaterialTable from 'material-table';
+
+/* Icon Imports */
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 const Facilites = () => {
+  const facilityData = Object.values(FACILITIES);
+  console.log(facilityData)
 
-  const {FACILITIES} = facs;
-  
+  const columnHeaders = [{title: 'ID', field: 'id'}, {title: 'Name', field: 'name'}, {title: 'Level', field: 'level'}, {title: 'Type', field: 'type'}]
+
   return (
     <div id="container">
       <Head>
         <title>pointSixtyFive - Facility Information</title>
       </Head>
 
-      <table>
-        <tbody>
-          <FacilityListItem facilities={FACILITIES} />
-        </tbody>
-      </table>
+      <MaterialTable
+        title='Facility'
+        columns={columnHeaders}
+        data={facilityData}
+        icons={tableIcons}
+        options={{sorting: true, filtering: true, search: false, paging: false}} 
+      />
     </div>
 )}
 
