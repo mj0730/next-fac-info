b4b6fcbbd357394d11115db2b06091582e66748f
Michael Johnson <mrj0730@icloud.com>
2020-05-13 17:25:37 -0500

    rename components, structure facility detail page

---
+++ components/DetailsStaffing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailsStaffing = props => (
+  <section id='fac-staffing'>
+    <p>This is the staffing section</p>
+
+  </section>
+
+)
+
+export default DetailsStaffing;
