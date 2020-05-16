f0cd864967f288b115b59595bfccef3403fe6f5a
Michael Johnson <mj@pointsixtyfive.com>
2020-05-16 13:43:52 -0500

    create error modal, basic styleing, context created; need to implement rendering

---
+++ components/ErrorModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorModal = () => {
+
+  return (
+    <div id="modal">
+      <p>'facid' is not a valid ID. Please use a vailid facility ID.</p>
+    </div>
+  )
+}
+
+export default ErrorModal;
