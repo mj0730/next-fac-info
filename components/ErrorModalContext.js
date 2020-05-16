8bb3721ebfeabfd09c2bcf8c75245eb0ee9761d9
Michael Johnson <mj@pointsixtyfive.com>
2020-05-16 13:43:52 -0500

    create error modal, basic styleing, context created; need to implement rendering

---
+++ components/ErrorModalContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from 'react';
+
+export const ErrorModalContext = createContext();
+
+const ErrorModalContextProvider = (props) => {
+  const [displayErrorModal, setDisplayErrorModal] = useState(false);
+
+  return (
+    <ErrorModalContext.Provider value={[displayErrorModal, setDisplayErrorModal]}>
+      {props.children}
+    </ErrorModalContext.Provider>
+  )
+}
+
+export default ErrorModalContextProvider;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import '../styles.css';
 import Layout from '../components/layout';
 import FacIdContextProvider from '../components/FacIdContext';
+import ErrorModalContext from '../components/ErrorModalContext';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <FacIdContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ErrorModalContext>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ErrorModalContext>
     </FacIdContextProvider>
   )
 }
