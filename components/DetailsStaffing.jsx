0594fcb15e404861afc84a8f984ba7552a7675fa
Michael Johnson <mj@pointsixtyfive.com>
2020-05-16 13:17:57 -0500

    added proptypes to faciliy info components

--- components/DetailsStaffing.jsx
+++ components/DetailsStaffing.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DetailsStaffing = props => (
   <section id='fac-staffing'>
-    <p>This is the staffing section</p>
+    <p>This is the staffing section. It is currently being developed</p>
 
   </section>
 
