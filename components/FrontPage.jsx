995010f9f61e3195ac29b671ea31beadeb86cc4b
Michael Johnson <mj@pointsixtyfive.com>
2020-05-17 02:59:26 -0500

    styling

--- components/FrontPage.jsx
+++ components/FrontPage.jsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 const FrontPage = () => {
 
   return (
-    <main>
+    <section id="front-page">
       <Head>
         <title>pointSixtyFive - Facility Information</title>
       </Head>
@@ -12,7 +12,7 @@ const FrontPage = () => {
       <p>Use the search box on the left to go directly to a facility's information.</p>
       <p>The 'Facilities' section has sortable/filterable information tables for all facilities.</p>
       <p>The facility map page is still under development.</p>
-    </main>
+    </section>
   )
 }
 
