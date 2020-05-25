0254ba22d9217e9100569d9006d4aaa25eebc16d
Michael Johnson <mj@pointsixtyfive.com>
2020-05-25 01:27:00 -0500

    refactor standardPayTable into smaller componenet

--- components/StandardPayTable.jsx
+++ components/StandardPayTable.jsx
@@ -1,18 +1,20 @@
+import StandardPayTableCell from './StandardPayTableCell';
+
 const StandardPayTable = ({basePay, cip}) => (
     <React.Fragment>
       <tbody>
         <tr>
-          <td>{basePay[0]} {cip[0] > 0 && <span className="cip">({cip[0]})</span>}</td>
-          {basePay[2] > 0 ? <td>{basePay[1]} {cip[1] > 0 && <span className="cip">({cip[1]})</span>}</td> : <td>N/A</td>}
-          {basePay[3] > 0 ? <td>{basePay[2]} {cip[2] > 0  && <span className="cip">({cip[2]})</span>}</td> : <td>N/A</td>}
-          {basePay[4] > 0 ? <td>{basePay[3]} {cip[3] > 0  && <span className="cip">({cip[3]})</span>}</td> : <td>N/A</td>}
+          <StandardPayTableCell basePay={basePay} cip ={cip} i = {0} />
+          {basePay[2] > 0 ? <StandardPayTableCell basePay={basePay} cip ={cip} i = {1} /> : <td>N/A</td>}
+          {basePay[3] > 0 ? <StandardPayTableCell basePay={basePay} cip ={cip} i = {2} /> : <td>N/A</td>}
+          {basePay[4] > 0 ? <StandardPayTableCell basePay={basePay} cip ={cip} i = {3} /> : <td>N/A</td>}
           <td rowSpan="2">{basePay[5]} {cip[5] > 0  && <span className="cip">({cip[5]})</span>}</td>
         </tr>
         <tr>
-          <td>{basePay[1]} {cip[1] > 0  && <span className="cip">({cip[1]})</span>}</td>
-          {basePay[2] > 0 ? <td>{basePay[2]} {cip[2] > 0  && <span className="cip">({cip[2]})</span>}</td> : <td>N/A</td>}
-          {basePay[3] > 0 ? <td>{basePay[3]} {cip[3] > 0  && <span className="cip">({cip[3]})</span>}</td> : <td>N/A</td>}
-          {basePay[4] > 0 ? <td>{basePay[4]} {cip[4] > 0  && <span className="cip">({cip[4]})</span>}</td> : <td>N/A</td>}
+        <StandardPayTableCell basePay={basePay} cip ={cip} i = {1} />
+          {basePay[2] > 0 ? <StandardPayTableCell basePay={basePay} cip ={cip} i = {1} /> : <td>N/A</td>}
+          {basePay[3] > 0 ? <StandardPayTableCell basePay={basePay} cip ={cip} i = {3} /> : <td>N/A</td>}
+          {basePay[4] > 0 ? <StandardPayTableCell basePay={basePay} cip ={cip} i = {4} />: <td>N/A</td>}
         </tr>
       </tbody>
     </React.Fragment>
