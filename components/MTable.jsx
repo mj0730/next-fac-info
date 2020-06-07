a1bdb1adc71852803d8e65919ff3c29b867b884b
Michael Johnson <mj@pointsixtyfive.com>
2020-06-07 14:31:36 -0500

    mui table initialized with dummy data

---
+++ components/MTable.jsx
@@ -0,0 +1,66 @@
+import MUIDataTable from 'mui-datatables';
+import {FacIdContext} from '../components/FacIdContext';
+import {FACILITIES} from '../scripts/facility_info';
+
+
+
+const MTable = () => {
+  const facilityData = Object.values(FACILITIES);
+
+  const columns = [
+    {
+     name: "name",
+     label: "Name",
+     options: {
+      filter: true,
+      sort: true,
+     }
+    },
+    {
+     name: "company",
+     label: "Company",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+    {
+     name: "city",
+     label: "City",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+    {
+     name: "state",
+     label: "State",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+   ];
+   
+   const data = [
+    { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+    { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+    { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+    { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
+   ];
+   
+   const options = {
+     filterType: 'checkbox',
+   };
+
+  return (
+    <MUIDataTable
+      title={'This is a Title'}
+      data={data}
+      columns={columns}
+      options={options}
+    />
+  )
+}
+
+export default MTable;
