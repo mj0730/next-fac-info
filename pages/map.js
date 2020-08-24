63e55fe9764d862c14b82fb6042d7b6c4c594e9c
Michael Johnson <mrj0730@icloud.com>
2020-08-23 23:11:28 -0500

    setup map

--- pages/map.js
+++ pages/map.js
@@ -1,17 +1,25 @@
 import Head from 'next/head';
+import React from 'react';
+import { Map } from 'react-leaflet';
+import { LatLngTuple } from 'leaflet';
+
 import { useState, useEffect, useContext } from 'react';
 import * as facs from '../scripts/facility_info';
+import L from 'leaflet';
 
-const Map = () => {
+const center = [40, -98.5];
+const zoom = 5;
 
+const LeafletMap = () => {
   return (
-    <div id="map-container">
+    <div id='map-container'>
       <Head>
         <title>pointSixtyFive - Facility and Locality Map</title>
       </Head>
 
-      <p>Map placeholder</p>
+      <Map id='map' center={center} zoom={zoom}></Map>
     </div>
-)}
+  );
+};
 
-export default Map;
+export default LeafletMap;
