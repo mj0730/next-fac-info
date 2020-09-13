2ec11145fdf1864fb051624a6ad78ffb7fff2377
Michael Johnson <mrj0730@icloud.com>
2020-09-12 19:29:21 -0500

    create staffing details table component; staffing data included on details page

--- components/DetailsStaffing.jsx
+++ components/DetailsStaffing.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DetailsStaffingTable from './DetailsStaffingTable';
 
-const DetailsStaffing = ({ data }) => (
-  <section id='fac-staffing'>
-    <h2>Staffing</h2>
-  </section>
-);
+function changeToPercentage(num) {
+  return `${(num * 100).toFixed(1)}%`;
+}
+
+const DetailsStaffing = ({ data }) => {
+  const cpcDetails = [
+    { text: 'Total', data: data['Current # of CPC On-Board'] },
+    { text: 'Target', data: data['CPC Target'] },
+    { text: '% Target', data: changeToPercentage(data['Current % CPC to Target']) },
+    { text: 'CPC-Trainee%', data: changeToPercentage(data['Current % CPC to Trainees']) },
+  ];
+
+  const cpcitDetails = [
+    { text: 'CPCIT0', data: data['CPCIT0'] },
+    { text: 'CPCIT1', data: data['CPCIT1'] },
+    { text: 'CPCIT2', data: data['CPCIT2'] },
+    { text: 'CPCIT3', data: data['CPCIT3'] },
+  ];
+
+  const devDetails = [
+    { text: 'AG', data: data['AG'] },
+    { text: 'Dev1', data: data['Dev1'] },
+    { text: 'Dev2', data: data['Dev2'] },
+    { text: 'Dev3', data: data['Dev3'] },
+  ];
+
+  return (
+    <section id='fac-staffing'>
+      <h2>Staffing</h2>
+      <DetailsStaffingTable data={cpcDetails} title={'CPC'} />
+      <DetailsStaffingTable data={cpcitDetails} title={'CPC-IT'} />
+      <DetailsStaffingTable data={devDetails} title={'Developmental'} />
+    </section>
+  );
+};
 
 export default DetailsStaffing;
 
@@ -17,9 +48,9 @@ DetailsStaffing.propTypes = {
 /*
 
 AG (SWB)
-ATCS in Training (SWB)
-ATCS in Training minus LTH (Calculation)
-ATCS in Training on Long Term Hold (SWB)
+ATCS in Training
+ATCS in Training minus LTH
+ATCS in Training on Long Term Hold
 CPC Target
 CPC on Long Term Hold (SWB)
 CPC on Temp (SWB)
@@ -31,9 +62,9 @@ CPCIT3 (SWB)
 Committed ATCS Inbound (SWB)
 Committed ATCS Outbound (SWB)
 CoreAirport
-Current # of CPC On-Board (SWB)
+Current # of CPC On-Board
 Current # of CPC On-Board minus Temps minus LTH (Calculation)
-Current % CPC to Target (Calculation)
+Current % CPC to Target
 Current % CPC to Trainees
 Current ATCS in Training Expected to Cert (Calculation)
 Dev Total (SWB)
