812b0eff47c551c19291e295b92014b7dba822c8
Michael Johnson <mrj0730@icloud.com>
2020-09-17 16:42:12 -0500

    front page complete

--- components/FrontPage.jsx
+++ components/FrontPage.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import Head from 'next/head';
 
 const FrontPage = () => {
-
   return (
-    <section id="front-page">
+    <section id='front-page'>
       <Head>
         <title>pointSixtyFive - Facility Information</title>
       </Head>
 
-      <p>Use the search box on the left to go directly to a facility's information.</p>
-      <p>The 'Facilities' section has sortable/filterable information tables for all facilities.</p>
-      <p>The facility map page is still under development.</p>
+      <h1>pointSixtyFive Facility Information</h1>
+
+      <div className='text-area'>
+        <p>Navigate to detailed facility pay, staffing, and training data with the search bar.</p>
+        <p>Search, filter, save, or print all facility data using the facilities tables.</p>
+        <p>Visualize federal locality areas and facility locations for all FAA facilities with the locality map.</p>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default FrontPage;
