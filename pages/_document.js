341cf8948da277679af3c58577589e734669593f
Michael Johnson <mrj0730@icloud.com>
2020-09-20 19:53:40 -0500

    edit meta to remove broken links temporarily

--- pages/_document.js
+++ pages/_document.js
@@ -26,17 +26,17 @@ class MyDocument extends Document {
           />
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
+          {/* <meta name='msapplication-config' content='/icons/browserconfig.xml' /> */}
           <meta name='msapplication-TileColor' content='#2B5797' />
           <meta name='msapplication-tap-highlight' content='no' />
           <meta name='theme-color' content='#000000' />
 
-          <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
-          <link rel='manifest' href='/static/manifest.json' />
-          <link rel='mask-icon' href='/static/icons/safari-pinned-tab.svg' color='#5bbad5' />
-          <link rel='shortcut icon' href='/static/icons/favicon.ico' />
+          {/* <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' /> */}
+          <link rel='manifest' href='/manifest.json' />
+          {/* <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' /> */}
+          {/* <link rel='shortcut icon' href='/icons/favicon.ico' /> */}
 
           <meta name='twitter:card' content='summary' />
           <meta name='twitter:url' content='https://pointsixtyfive.com' />
@@ -45,7 +45,7 @@ class MyDocument extends Document {
             name='twitter:description'
             content='pointSixtyFive.com tool for finding and searching FAA facility information'
           />
-          <meta name='twitter:image' content='https://app.pointsixtyfive.com/static/icons/android-chrome-192x192.png' />
+          {/* <meta name='twitter:image' content='https://app.pointsixtyfive.com/icons/android-chrome-192x192.png' /> */}
           <meta property='og:type' content='website' />
           <meta property='og:title' content='pointSixtyFive Facility Information' />
           <meta
@@ -54,7 +54,7 @@ class MyDocument extends Document {
           />
           <meta property='og:site_name' content='pointSixtyFive' />
           <meta property='og:url' content='https://pointsixtyfive.com' />
-          <meta property='og:image' content='https://app.pointsixtyfive.com/static/icons/apple-touch-icon.png' />
+          {/* <meta property='og:image' content='https://app.pointsixtyfive.com/icons/apple-touch-icon.png' /> */}
         </Head>
 
         <body>
