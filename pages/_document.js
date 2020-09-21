2d14719ef6b433504f182fe223b7fed89e95b0d5
Michael Johnson <mrj0730@icloud.com>
2020-09-20 20:42:40 -0500

    add icons and related meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -27,16 +27,22 @@ class MyDocument extends Document {
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
           {/* <meta name='msapplication-config' content='/icons/browserconfig.xml' /> */}
-          <meta name='msapplication-TileColor' content='#2B5797' />
+          <meta name='msapplication-TileColor' content='#14518A' />
           <meta name='msapplication-tap-highlight' content='no' />
           <meta name='theme-color' content='#000000' />
 
-          {/* <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' /> */}
           <link rel='manifest' href='/manifest.json' />
-          {/* <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' /> */}
-          {/* <link rel='shortcut icon' href='/icons/favicon.ico' /> */}
+          <link rel='apple-touch-icon' sizes='192x192' href='/icons/icon-192x192.png' />
+          <link rel='icon' type='image/png' sizes='72x72' href='/icons/icon-72x72.png' />
+          <link rel='icon' type='image/png' sizes='96x96' href='/icons/icon-96x96.png' />
+          <link rel='icon' type='image/png' sizes='128x128' href='/icons/icon-128x128.png' />
+          <link rel='icon' type='image/png' sizes='144x144' href='/icons/icon-144x144.png' />
+          <link rel='icon' type='image/png' sizes='152x152' href='/icons/icon-152x152.png' />
+          <link rel='icon' type='image/png' sizes='192x192' href='/icons/icon-192x192.png' />
+          <link rel='icon' type='image/png' sizes='384x384' href='/icons/icon-384x384.png' />
+          <link rel='icon' type='image/png' sizes='512x512' href='/icons/icon-512x512.png' />
+          <link rel='mask-icon' href='/icons/mask-icon.svg' color='#14518A' />
+          <link rel='shortcut icon' href='/favicon.ico' />
 
           <meta name='twitter:card' content='summary' />
           <meta name='twitter:url' content='https://pointsixtyfive.com' />
@@ -45,7 +51,7 @@ class MyDocument extends Document {
             name='twitter:description'
             content='pointSixtyFive.com tool for finding and searching FAA facility information'
           />
-          {/* <meta name='twitter:image' content='https://app.pointsixtyfive.com/icons/android-chrome-192x192.png' /> */}
+          <meta name='twitter:image' content='https://app.pointsixtyfive.com/icons/icon-192x192.png' />
           <meta property='og:type' content='website' />
           <meta property='og:title' content='pointSixtyFive Facility Information' />
           <meta
@@ -54,7 +60,7 @@ class MyDocument extends Document {
           />
           <meta property='og:site_name' content='pointSixtyFive' />
           <meta property='og:url' content='https://pointsixtyfive.com' />
-          {/* <meta property='og:image' content='https://app.pointsixtyfive.com/icons/apple-touch-icon.png' /> */}
+          <meta property='og:image' content='https://app.pointsixtyfive.com/icons/icon-192x192.png' />
         </Head>
 
         <body>
