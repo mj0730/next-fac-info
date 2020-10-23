b913a0350c05f2a80bb42429a154a840a6021f0a
Michael Johnson <hack.reactor.mj0730@gmail.com>
2020-10-23 14:28:17 -0500

    add forum link and related styling to facility info page; add styling to better indicate a link on the datatables; remove unneeded prop (table specific theme) being passed to datatables and associated logic as its no longer needed

--- components/DetailsInformation.jsx
+++ components/DetailsInformation.jsx
@@ -1,26 +1,35 @@
 import PropTypes from 'prop-types';
 
-const DetailsInformation = ({ currentFacInfo }) => (
-  <section id='fac-information'>
-    <div className='flex-row'>
-      <div className='fac-information-item address'>
-        {currentFacInfo.address}, {currentFacInfo.city}, {currentFacInfo.state} {currentFacInfo.zip}
+const DetailsInformation = ({ currentFacInfo }) => {
+  const facilityURL = `https://pointsixtyfive.com/wiki/${currentFacInfo['Facility ID']}`;
+
+  return (
+    <section id='fac-information'>
+      <div className='flex-row'>
+        <div className='fac-information-item address'>
+          {currentFacInfo.address}, {currentFacInfo.city}, {currentFacInfo.state} {currentFacInfo.zip}
+        </div>
       </div>
-    </div>
 
-    <div className='flex-row'>
-      <div className='fac-information-item'>Service Area: {currentFacInfo['Service Area']}</div>
-      <div className='fac-information-item'>District: {currentFacInfo['District']}</div>
-      <div className='fac-information-item'>Region: {currentFacInfo['HRRegion']}</div>
-    </div>
+      <div className='flex-row'>
+        <div className='fac-information-item'>Service Area: {currentFacInfo['Service Area']}</div>
+        <div className='fac-information-item'>District: {currentFacInfo['District']}</div>
+        <div className='fac-information-item'>Region: {currentFacInfo['HRRegion']}</div>
+      </div>
 
-    <div className='flex-row'>
-      <div className='fac-information-item'>{currentFacInfo.phone}</div>
-      <div className='fac-information-item'>{currentFacInfo.hours}</div>
-      <div className='fac-information-item'>Travel Days: {currentFacInfo.travelDays}</div>
-    </div>
-  </section>
-);
+      <div className='flex-row'>
+        <div className='fac-information-item'>{currentFacInfo.phone}</div>
+        <div className='fac-information-item'>{currentFacInfo.hours}</div>
+        <div className='fac-information-item'>Travel Days: {currentFacInfo.travelDays}</div>
+        <div className='fac-information-item'>
+          <a href={facilityURL} target='_blank' rel='noreferrer' className='forum-link'>
+            Forum Page
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default DetailsInformation;
 
