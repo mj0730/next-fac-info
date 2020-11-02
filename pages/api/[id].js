122bc958b3a806be9278b3823c89f533fdb3d68e
Michael Johnson <hack.reactor.mj0730@gmail.com>
2020-11-02 16:26:52 -0600

    add error handling for nonexistant id

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -2,6 +2,7 @@ const placeholder = {
   D10: { id: 'D10', name: 'DFW TRACON', level: 12 },
   MAF: { id: 'MAF', name: 'Midland', level: 7 },
 };
+// query parameter in the route is case sensitive
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
@@ -9,7 +10,14 @@ export default function handler(req, res) {
       query: { id },
     } = req;
 
-    const result = placeholder[id];
+    let result;
+    if (placeholder[id]) {
+      result = placeholder[id];
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Error: No ID with that value found.');
+    }
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
