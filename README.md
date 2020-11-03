c30d2f0823b5f4ace751453c3115c89e1443aaf7
Michael Johnson <mrj0730@icloud.com>
2020-11-02 21:13:55 -0600

    complete endpoints; add documentation for api to readme

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Progressive web app made with React + Next.js
 
 Tables created with [mui-datatables](https://github.com/gregnb/mui-datatables)  
 Map created with [Leaflet.js](https://leafletjs.com/)
+
+## API
+
+Facility information can be accessed in it's entirety or by individual facility.
+
+### Endpoints
+
+`/api/data` - JSON data of all facility information
+`/api/[id]` - Use a facility's 3-letter identifier to access facility specific information.
--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,22 +1,21 @@
-const placeholder = {
-  D10: { id: 'D10', name: 'DFW TRACON', level: 12 },
-  MAF: { id: 'MAF', name: 'Midland', level: 7 },
-};
+import { getDataFromDatabase } from './data';
+
 // query parameter in the route is case sensitive
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    const {
-      query: { id },
-    } = req;
+export default async function handler(req, res) {
+  const { data, payScaleData } = await getDataFromDatabase();
 
-    let result;
-    if (placeholder[id]) {
-      result = placeholder[id];
+  if (req.method === 'GET') {
+    const id = req.query.id.toUpperCase();
+    const facility = {};
+
+    if (data[id]) {
+      facility.data = data[id];
+      facility.pay = payScaleData[id];
 
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(result));
+      res.end(JSON.stringify(facility));
     } else {
       res.statusCode = 404;
       res.setHeader('Content-Type', 'text/plain');
