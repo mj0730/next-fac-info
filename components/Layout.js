69794a90ed243f0e4eb858be9dd9f6c16057c39d
Michael Johnson <hack.reactor.mj0730@gmail.com>
2020-11-06 16:21:35 -0600

    add left margin to seach box on small screens; add grid-gap below facility info header on small screens; update mongodb dependency

--- components/Layout.js
+++ components/Layout.js
@@ -15,7 +15,7 @@ function Layout({ children }) {
       <div className='index-display-column'>{children}</div>
 
       <footer className='index-footer'>
-        <img src='circle_logo.png' width='24' height='24' alt='Logo' />
+        <img src='images/circle_logo.png' width='24' height='24' alt='Logo' />
         <a href='https://pointsixtyfive.com'>pointSixtyFive.com</a>
       </footer>
     </div>
