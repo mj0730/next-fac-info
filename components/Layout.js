1c4fdbbbf2494c5f0ea441c2b14e4d1417a29ac4
Michael Johnson <mrj0730@icloud.com>
2021-06-17 14:27:16 -0500

    fix weback error importing version info from package.json in layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import PageLinks from './PageLinks';
 import SearchBox from './SearchBox';
-import { version } from '../package.json';
+import packageInfo from '../package.json';
 
 function Layout({ children }) {
   return (
@@ -9,7 +9,7 @@ function Layout({ children }) {
       <div className='index-nav-column'>
         <SearchBox />
         <PageLinks />
-        <div className='version'>v{version}</div>
+        <div className='version'>v{packageInfo.version}</div>
       </div>
 
       <div className='index-display-column'>{children}</div>
